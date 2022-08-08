4bba20beff4d5866d4cf44d1e9c951c86523f5d4
Matheus Marinho <matheus.marinho@manipulae.com.br>
2022-08-08 09:23:03 -0300

    fix: testing mirage on deployment vercel

--- src/modules/home/business/budget.ts
+++ src/modules/home/business/budget.ts
@@ -4,11 +4,11 @@ interface IBudgetBusiness {
 
 export default function BudgetBusiness({ initialRender } : IBudgetBusiness) {
   if (initialRender) {
-    fetch('http://localhost:3000/api/budget')
+    fetch('https://vibbra-challenge.vercel.app/api/budget')
       .then(data => data.json())
       .then(data => console.log(data))
     
-    fetch('http://localhost:3000/api/preferences')
+    fetch('https://vibbra-challenge.vercel.app/api/preferences')
       .then(data => data.json())
       .then(data => console.log(data))
   }
